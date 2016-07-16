c228a573e9d2d41ccd66baece151b581db98e4a4
olagz <olagodavivid@gmail.com>
2016-07-16 21:56:00 +0300

    changes and updates

--- js/app.js
+++ js/app.js
@@ -1,23 +1,19 @@
 $(document).foundation()
 
-{
-  "name": "socket-chat-example",
-  "version": "0.0.1",
-  "description": "my first socket.io app",
-  "dependencies": {}
-}
-
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
-});
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
 });
